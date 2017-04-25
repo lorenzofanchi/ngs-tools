e6b0d960e743b4bae1648f78c092904e7c01f683
Lorenzo Fanchi <l.fanchi@nki.nl>
2017-04-25 15:32:10 +0200

    update function arguments

--- dnaseq.R
+++ dnaseq.R
@@ -15,24 +15,22 @@ bam_files = list.files(path = file.path(data_path, '1c_dnaseq_data/bam'),
 											 full.names = TRUE) %>% grep('-bq', ., invert = T, value = T)
 
 invisible(sapply(bam_files,
-								 function(file) performBaseQualityRecalibrationUsingGatk(bam = file,
-								 																												bed_regions = )))
+								 function(file) performBaseQualityRecalibrationUsingGatk(bam = file)))
 
 # perform variant calling
 bam_files = list.files(path = file.path(data_path, '1c_dnaseq_data/bam'),
 											 pattern = '\\-bq.bam$',
 											 full.names = TRUE)
 
-invisible(mapply(function(normal, tumor) callSomaticVariantsUsingGatkMutect2(normal_bam = ,
-								 																									 tumor_bam = ,
-								 																									 bed_regions = ),
+invisible(mapply(function(normal, tumor) callSomaticVariantsUsingGatkMutect2(normal_bam = normal,
+																																						 tumor_bam = tumor,
+																																						 bed_regions = ),
 								 bam_files[seq(1, length(bam_files), 2)],
 								 bam_files[seq(2, length(bam_files), 2)])) ## check that this works out for you paired samples!
 
 slopCoordinatesUsingBedtools(vcf = )
 
 callGermlineVariantsUsingGatkHaplotypeCaller(normal_bam = ,
-																						 n_threads = 1,
 																						 bed_regions = )
 
 
